d4a3ae7c0d44d1bad5c2a384f60b2e956cc1056f
mtaracha <marcin.taracha@jobsite.co.uk>
2018-06-22 16:38:24 +0100

    Trigger build

--- infrastructure/dev/main.tf
+++ infrastructure/dev/main.tf
@@ -1,19 +1,20 @@
 /*
-  ECS Service definition with its dependencies
+  ECS Service definition
 */
 
 module "stepstone-techradar" {
   source = "git::ssh://git@github.com/trufa-me/terraformers.git//modules/ecs_service?ref=ecs_service-latest"
 
-  service_name   = "stepstone-techradar"
-  cluster_type   = "general-purpose" # Options - ["memory-optimized" or "general-purpose"] choose depending on workload type - if it's memory heavy choose memory-optimized, if you now sure - choose general purpose
-  container_port = "80"
-  image_tag      = "dev-latest"
-  alb_listener_rule_priority = "41522" # "priority" must be in the range 1-99999
+  service_name               = "stepstone-techradar"
+  cluster_type               = "general-purpose"     # Options - ["memory-optimized" or "general-purpose"] choose depending on workload type - if it's memory heavy choose memory-optimized, if you now sure - choose general purpose
+  container_port             = "80"
+  image_tag                  = "dev-latest"
+  alb_listener_rule_priority = "41522"               # "priority" must be in the range 1-99999
 
   # Service resource allocation
   #cpu = "0" # The number of cpu units to reserve for the container - https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_definition_parameters.html
   memory = "1024" # memory allocation for single service container
+
   desired_count = "2" # Desired count of service containers
 
   # Service to loadbalancer registration - configuration options
@@ -26,23 +27,23 @@ module "stepstone-techradar" {
   #tg_health_interval = "30" # interval between health check requests in seconds
 
   # terraform chained parameters
-  tf_state_key    = "dev_env_dev_common.tfenv"
+  tf_state_key = "dev_env_dev_common.tfenv"
 }
 
 /*
   Additional configurations
 */
 
+// This module requires specific terraform version
+module "terraform_version" {
+  source = "git::ssh://git@github.com/trufa-me/terraformers.git//modules/terraform_version?ref=terraform_version-0.10.7"
+}
 
- // This module requires specific terraform version
- module "terraform_version" {
-   source = "git::ssh://git@github.com/trufa-me/terraformers.git//modules/terraform_version?ref=terraform_version-0.10.7"
- }
- // Terraform state file specific for service
- terraform {
-     backend "s3" {
-     bucket = "terraform-state-stepweb-dev"
-       key    = "services/stepstone-techradar.tfenv"
-       region = "eu-west-1"
-     }
- }
+// Terraform state file specific for service
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-stepweb-dev"
+    key    = "services/stepstone-techradar.tfenv"
+    region = "eu-west-1"
+  }
+}
